02210d947031b1d1507735035f139b21215e7437
Dev_Osmium <Oatmas64134@gmail.com>
2017-12-24 15:12:42 -0500

    Begin Testing Security Commands

--- src/main/java/net/devosmium/adhubbot/SecurityCommands.java
+++ src/main/java/net/devosmium/adhubbot/SecurityCommands.java
@@ -12,7 +12,7 @@ public class SecurityCommands {
 
 
     static {
-        commandMap.put("test", (event, args) -> {
+        commandMap.put("sectest", (event, args) -> {
             PermLevel userPerms = getUserPermLevel(event);
 
             switch (userPerms) {
@@ -67,19 +67,12 @@ public class SecurityCommands {
         argsList.remove(0); // Remove the command
 
         // Instead of delegating the work to a switch, automatically do it via calling the mapping if it exists
-        boolean testMode = true;
-        if (testMode) {
-            if (event.getAuthor().getStringID().equals("118455061222260736")) {
-                if (commandMap.containsKey(commandStr))
-                    commandMap.get(commandStr).runCommand(event, argsList);
-            } else {
-                BotUtils.sendMessage(event.getChannel(), "Testing mode is engaged. Please" +
-                        " hold.", "Sorry!", event, false);
-            }
-        } else {
+
+
+
             if (commandMap.containsKey(commandStr))
                 commandMap.get(commandStr).runCommand(event, argsList);
-        }
+
 
     }
 
